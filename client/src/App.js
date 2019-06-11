d59970dbed7303fc808ed384b20a01f90c590ecf
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-06-11 12:24:44 +0100

    Refactor app js to use hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,31 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 
-export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      savedList: []
-    };
-  }
+const App = () => {
+  const [savedList, setSavedList] = useState([]);
 
-  addToSavedList = movie => {
-    const savedList = this.state.savedList;
-    savedList.push(movie);
-    this.setState({ savedList });
+  const addToSavedList = movie => {
+    setSavedList([...savedList, movie]);
   };
 
-  render() {
-    return (
-      <Router>
-        <div>
-          <SavedList list={this.state.savedList} />
-          <Route exact path='/' component={MovieList} />
-          <Route 
-          path='/movies/:id' 
-          // component={Movie} 
-            render={(routeProps) => (
-              <Movie {...routeProps} addToSavedList={this.addToSavedList} />
-            )}
-          />
-        </div>
-      </Router>
-    );
-  }
-}
+  return (
+    <Router>
+      <div>
+        <SavedList list={savedList} />
+        <Route exact path="/" component={MovieList} />
+        <Route
+          path="/movies/:id"
+          render={routeProps => (
+            <Movie {...routeProps} addToSavedList={addToSavedList} />
+          )}
+        />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
